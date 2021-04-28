a33ff081946276a86333852451ce4c57561ebc32
Dario Evers <darioevers@gmail.com>
2021-04-28 14:20:54 +0200

    added functionalty for password generator

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ const passGenerator = () => {
   for (let i = 0; i < length; i++) {
     password += charSet.charAt(Math.floor(Math.random() * charSet.length));
   }
+  return password;
   console.log(`This is your randomly generated password: ${password}`);
 };
 
-passGenerator();
+function generator() {
+  document.getElementById("password_field").value = passGenerator();
+}
